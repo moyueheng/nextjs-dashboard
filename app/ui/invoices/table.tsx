ff93417e63e37655075565b219ce09918cef90eb
莫岳恒 <moyueheng@datagrand.com>
2024-03-17 20:52:37 +0800

    feat: ✨ 实现搜索和分页

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,8 +11,11 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  console.log(`query: ${query}`);
   const invoices = await fetchFilteredInvoices(query, currentPage);
-
+  console.log("updating");
+  console.log(invoices);
+  console.log("updated");
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
